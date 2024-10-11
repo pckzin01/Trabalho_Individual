e6e632634ae30ba993afd32c999ce5912dc6b6d6
pckzin01 <patrickgoncalvesdepaiva@gmail.com>
2024-10-11 00:34:31 -0300

    inicialização do projeto maven

---
+++ TrabalhoIndividual/src/test/java/br/org/serratec/TrabalhoIndividual/TrabalhoIndividualApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.serratec.TrabalhoIndividual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoIndividualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
